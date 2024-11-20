7531c09fc9ef194fc1001dc87c479828650a7f65
henry li <lih7>
2024-11-20 16:42:52 -0500

    some starting stuff

---
+++ settings.py
@@ -0,0 +1,9 @@
+OBSERVE_FPS = 0.5
+OBSERVE_PR = 1
+OBSERVE_ISSUE = 1
+OBSERVE_RELEASE = 60
+
+
+
+print("")
+print(f"Checking {round(OBSERVE_FPS*60*100)/100} times per minute (every {round(1/OBSERVE_FPS*100)/100} seconds)")
---
+++ test.py
@@ -0,0 +1,29 @@
+import requests
+
+response = requests.get("https://api.github.com/repos/wpilibsuite/allwpilib/pulls")
+print(response.status_code)
+
+info = response.json()
+
+for pr in info:
+    print(pr["title"])
+
+
+class DiscordWebhook:
+    def __init__(self,url,webhook="    "):
+        self.url=url
+        self.name=webhook
+        self.avatar=""
+    def sendmessage(self,message):
+        data={"content":(str(message)),"username":self.name,"avatar_url":self.avatar}
+        response=requests.post(self.url,json=data)
+        print(self, response,response.status_code,response.request.body)
+    def sendrawmessage(self,rawmessage):
+        data=rawmessage
+        response=requests.post(self.url,json=data)
+        print(self, response,response.status_code,response.request.body)
+    def rename(self,name):
+        self.name=str(name)
+    def reavatar(self,avatarlink):
+        self.avatar=avatarlink
+
